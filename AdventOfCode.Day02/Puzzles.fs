ae2c5515543396223d68f0f36ac7a54070f2003a
Marton Balogh Bodor <marton2@gmail.com>
2020-12-02 14:43:16 +0100

    Simplify solution

--- AdventOfCode.Day02/Puzzles.fs
+++ AdventOfCode.Day02/Puzzles.fs
@@ -6,13 +6,9 @@ module Puzzles =
     let describe = 
         "Password Philosophy"
 
-    let rec checkPassword1 (min, max, chr, (lst: char list)) curr =
-        if lst.IsEmpty then min <= curr && curr <= max
-        else
-            let head = lst.Head
-            let next = if head = chr then curr + 1 else curr
-
-            checkPassword1 (min, max, chr, lst.Tail) next
+    let rec checkPassword1 (min, max, chr, (lst: char list)) =
+        let count = lst |> List.filter(fun x -> x = chr) |> List.length
+        min <= count && count <= max
 
     let rec checkPassword2 (pos1, pos2, chr, (lst: char list)) =
         let idx1 = pos1 - 1
@@ -28,7 +24,7 @@ module Puzzles =
                            |> List.map(fun x -> regex.Match(x)) 
                            |> List.map(fun x -> (Operators.int(x.Groups.[1].Value), Operators.int(x.Groups.[2].Value), Operators.char(x.Groups.[3].Value), [for c in x.Groups.[4].Value -> c]))
 
-        let valid1 = policies |> List.filter(fun x -> checkPassword1 x 0) |> List.length
+        let valid1 = policies |> List.filter(fun x -> checkPassword1 x) |> List.length
         let valid2 = policies |> List.filter(fun x -> checkPassword2 x) |> List.length
 
         printfn "Total passwords: %d" policies.Length
