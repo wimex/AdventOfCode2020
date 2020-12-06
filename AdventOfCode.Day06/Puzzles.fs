1ae36589cba555ffd7b3f8b3cf6805a9cf84b7dd
Marton Balogh Bodor <marton2@gmail.com>
2020-12-06 21:12:49 +0100

    Solutions for day 6

---
+++ AdventOfCode.Day06/Puzzles.fs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode.Day06
+
+module Puzzles =
+    let describe =
+        "Custom Customs"
+
+    let intersect (string1: string) (string2: string) =
+        System.String.Join("", System.Linq.Enumerable.Intersect(string1 |> List.ofSeq, string2 |> List.ofSeq))
+
+    let execute (filenames: string list) =
+        let lines = System.IO.File.ReadAllText(filenames.Head)
+        let groups = lines.Split("\n\n", System.StringSplitOptions.RemoveEmptyEntries)
+        
+        let unique1 = groups |> Seq.map(fun (l: string) -> l.Replace("\n", "")) |> List.ofSeq |> List.map(fun l -> l |> Seq.distinct |> Seq.length) |> Seq.reduce((+))
+        let unique2 = groups |> Seq.map(fun (l: string) -> l.Split("\n", System.StringSplitOptions.RemoveEmptyEntries) |> Seq.reduce(fun p e -> intersect p e) |> Seq.length) |> Seq.reduce((+))
+
+        printfn "Sum of unique values for question 1: %d" unique1
+        printfn "Sum of unique values for question 2: %d" unique2
