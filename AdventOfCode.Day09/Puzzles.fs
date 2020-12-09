a51d399a7e2e03255d6b2c90d4d83908e6b20b60
Marton Balogh Bodor <marton2@gmail.com>
2020-12-09 14:20:56 +0100

    Solutions for day 9

---
+++ AdventOfCode.Day09/Puzzles.fs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode.Day09
+
+module Puzzles =
+    let describe =
+        "Encoding Error"
+
+    let rec findOutlier (numbers: int64[]) ws we =
+        let head = numbers.[we + 1]
+        let exists = numbers.[ws..we] 
+                        |> Seq.collect(fun x -> numbers.[ws..we] |> Seq.map(fun y -> int64(x + y)) |> Seq.filter((=) head))
+                        |> Seq.length = 0
+
+        if exists then head
+        else findOutlier numbers (ws + 1) (we + 1)
+        
+    let rec findSumsOfSize (numbers: int64[]) target position length =
+        if (position + length) >= numbers.Length then (false, Array.empty)
+        else
+            let window = numbers.[position..(position + length)]
+            let sum = window |> Seq.sum
+
+            if sum = target then (true, window)
+            else findSumsOfSize numbers target (position + 1) length
+
+    let rec findSums (numbers: int64[]) target length =
+        let (found, window) = findSumsOfSize numbers target 0 length
+        if found then window
+        else findSums numbers target (length + 1)
+        
+    let execute (filenames: string list) =
+        let lines = System.IO.File.ReadAllLines(filenames.Head) |> Seq.map(int64) |> Array.ofSeq
+        
+        let outlier = findOutlier lines 0 24
+        let weakness = findSums lines outlier 2
+        let wmin = weakness |> Seq.min
+        let wmax = weakness |> Seq.max
+
+        printfn "The first invalid number is %d" outlier
+        printfn "Encryption weakness: %d + %d = %d" wmin wmax (wmin + wmax)
--- AdventOfCode.Runner/Program.cs
+++ AdventOfCode.Runner/Program.cs
@@ -12,7 +12,8 @@ namespace AdventOfCode.Runner
         private static readonly Dictionary<int, Type> Solutions =new Dictionary<int, Type>
         {
             {1, typeof(Day01.Puzzles)}, {2, typeof(Day02.Puzzles)}, {3, typeof(Day03.Puzzles)}, {4, typeof(Day04.Puzzles)},
-            {5, typeof(Day05.Puzzles)}, {6, typeof(Day06.Puzzles)}, {7, typeof(Day07.Puzzles)}, {8, typeof(Day08.Puzzles)}
+            {5, typeof(Day05.Puzzles)}, {6, typeof(Day06.Puzzles)}, {7, typeof(Day07.Puzzles)}, {8, typeof(Day08.Puzzles)},
+            {9, typeof(Day09.Puzzles)}
         };
 
         public static void Main(string[] args)
